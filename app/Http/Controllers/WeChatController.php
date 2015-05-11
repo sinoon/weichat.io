df45b913577245f7e44911a445f6f5c740d23daa
沧浪 <sinoon1218@gmail.com>
2015-05-11 17:14:05 +0800

    修改 修改 。。

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -17,42 +17,13 @@ class WeChatController extends Controller {
      *
      * @return string
      */
-    public function test()
+    public function serve(Server $server)
     {
-        $appId = '***';
-        $appSecret = '***';
-        $token = '***';
-        $encodingAESKey = '***';
-
-        $server = new Server($appId, $token, $encodingAESKey);
-
-        // 监听所有类型
-        $server->on('message', function ($message) {
-            Log::error($message);
-            return Message::make('text')->content('您好！');
+        $server->on('message', function($message){
+            return "欢迎关注 overtrue！";
         });
 
-        // 监听指定类型
-        $server->on('message', 'image', function ($message) {
-            return Message::make('text')->content('我们已经收到您发送的图片！');
-        });
-
-        // 监听所有事件
-        $server->on('event', function ($event) {
-            Log::error('收到取消关注事件，取消关注者openid: ' . $event['FromUserName']);
-        });
-
-        // 只监听指定类型事件
-        $server->on('event', 'subscribe', function ($event) {
-
-            Log::error('收到关注事件，关注者openid: ' . $event['FromUserName']);
-
-            return Message::make('text')->content('感谢您关注');
-        });
-
-        $result = $server->serve();
-
-        echo $result;
+        return $server->serve(); // 或者 return $server;
     }
 
 }
