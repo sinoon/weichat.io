40e7900dce47997c706113e2224a6bbd1e6c6681
sinoon <sinoon1218@gmail.com>
2015-08-18 19:08:24 +0800

    * 增加邮件工具

---
+++ Utils/mailer.js
@@ -0,0 +1,28 @@
+/**
+ * Created by sinoon on 15/8/18.
+ */
+
+var nodemailer = require('nodemailer');
+
+var transport = nodemailer.createTransport({
+	use_authentication : true,
+	host : 'smtp.qq.com',
+	port : 25,
+	ssl : true,
+	auth : {
+		user : "shangnan@qwbcg.com",
+		pass : "xxsnkkll1218"
+	}
+});
+
+module.exports = function ( form,to,subject,text,callback ) {
+	transport.sendMail({
+		form:form,
+		to:to,
+		subject:subject,
+		text:text
+	}, function ( err,info ) {
+
+		return callback(err,info)
+	})
+};
