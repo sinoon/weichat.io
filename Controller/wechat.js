0569377dd5ac0ddb8528c461537ee29ae6200a37
sinoon <sinoon1218@gmail.com>
2015-08-18 12:15:47 +0800

    * 测试

---
+++ Controller/wechat.js
@@ -0,0 +1,28 @@
+/**
+ * Created by sinoon on 15/8/18.
+ */
+
+var wechat = require('wechat');
+var request  = require('request');
+
+var robotUrl = 'http://www.tuling123.com/openapi/api';
+var robotKey = '11607077906726b66072dda871205cd6';
+
+var config = {
+	token:'123abc',
+	appid:'wx6ec63f5b355e74fb'
+};
+
+module.exports = wechat(config,function ( req,res,next ) {
+	var message = req.weixin;
+
+	var content = message.content;
+
+	request.get(robotUrl,{
+		key:robotKey,
+		info:content
+	}, function ( error,resopnse,body ) {
+		console.log(body);
+		res.reply(body)
+	})
+});
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var wechat = require('wechat');
+var wechat = require('./Controller/wechat');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -30,19 +30,9 @@ app.use('/users', users);
 app.use('/test', test);
 app.use('/interface',inter);
 
-var config = {
-    token:'123abc',
-    appid:'wx6ec63f5b355e74fb'
-};
+
 app.use(express.query());
-app.use('/wechat',wechat(config, function ( req,res,next ) {
-    var message = req.weixin;
-
-    console.log(message);
-    console.log(message.FromUserName);
-    res.reply('你好，' + message.FormUserName)
-
-}));
+app.use('/wechat',wechat);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
