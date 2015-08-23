03cc7a9ce412df048a3c52222983d95499ea2f93
sinoon <sinoon1218@gmail.com>
2015-08-23 18:41:23 +0800

    * 修改签名生成方式

--- routes/update.js
+++ routes/update.js
@@ -7,35 +7,32 @@ var router = express.Router();
 
 var shell = require('shelljs');
 var crypto = require('crypto');
-var bl = require('bl');
 
 router.post('/', function ( req,res,next ) {
 
 	console.log(req);
 
-	req.pipe(bl( function ( err,data ) {
 
-		var hmac = crypto.createHmac('sha1','abcdefg');
-		hmac.update(JSON.stringify(data));
-		var signature = 'sha1=' + hmac.digest('hex');
+	var hmac = crypto.createHmac('sha1','abcdefg');
+	hmac.update(JSON.stringify(req.body));
+	var signature = 'sha1=' + hmac.digest('hex');
 
-		var _signature = req.headers['x-hub-signature'];
+	var _signature = req.headers['x-hub-signature'];
 
-		console.log(signature);
-		console.log(_signature);
+	console.log(signature);
+	console.log(_signature);
 
-		if(req.headers['x-hub-signature'] != signature){
-			console.log('匹配失败');
-			return res.end()
-		}
+	if(req.headers['x-hub-signature'] != signature){
+		console.log('匹配失败');
+		return res.end()
+	}
 
-		res.end('ok');
+	res.end('ok');
 
-		shell.pwd();
-		shell.exec('git pull');
-		shell.exec('pm2 restart all')
+	shell.pwd();
+	shell.exec('git pull');
+	shell.exec('pm2 restart all')
 
-	}));
 });
 
 module.exports = router;
