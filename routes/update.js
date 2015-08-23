ee1ed132ca97927e9c90e18d8c67d2dad6c24d6c
sinoon <sinoon1218@gmail.com>
2015-08-23 17:34:52 +0800

    * 增加对于github发送过来信息的验证

--- routes/update.js
+++ routes/update.js
@@ -6,10 +6,20 @@ var express = require('express');
 var router = express.Router();
 
 var shell = require('shelljs');
+var crypto = require('crypto');
 
 router.post('/', function ( req,res,next ) {
 
 	console.log(req);
+
+	var hmac = crypto.createHmac('sha1','1234567890');
+	hmac.update(JSON.parse(req.body));
+	var signature = 'sha1' + hmac.digest('hex');
+
+	if(req.headers['x-hub-signature'] != signature){
+		return res.end()
+	}
+
 	res.end('ok');
 
 	shell.pwd();
